f1c40e771e8cab83569a4cabc052a1e532e6c5dd
Ramkumaraventurine <ramkumar.m@aventurine.co.in>
2023-12-08 05:34:09 +0530

    Revert "ionic added"
    This reverts commit d72d988b742bf0acd78e73aac61fb7c3e8dd30df.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { WorkspacedetailsComponent } from './pages/workspace/workspacedetails/workspacedetails.component';
 
 const routes: Routes = [
+  
   {
     path:'',
     redirectTo:'login',
